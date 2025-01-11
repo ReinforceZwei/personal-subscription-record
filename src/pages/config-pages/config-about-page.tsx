8b4deba8d1a34f3a95679078bdf5d8e9f08e9f9d
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2025-01-11 21:32:17 +0800

    service worker not even exist in pwa

--- src/pages/config-pages/config-about-page.tsx
+++ src/pages/config-pages/config-about-page.tsx
@@ -3,13 +3,6 @@ import { useEffect, useState } from "react"
 
 export default function ConfigAboutPage() {
     const loadedScripts = Array.from(document.getElementsByTagName('script'))
-    const [swCount, setSwCount] = useState(0)
-
-    useEffect(() => {
-        navigator.serviceWorker.getRegistrations().then((regs) => {
-            setSwCount(regs.length)
-        })
-    }, [])
 
     const reload = () => {
         window.location.reload()
@@ -17,16 +10,6 @@ export default function ConfigAboutPage() {
 
     const clearCacheAndReload = () => {
         window.location.reload(true)
-    }
-
-    const forceSwUpdate = () => {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.getRegistrations().then(function (registrations) {
-                for (let registration of registrations) {
-                    registration.update()
-                }
-            })
-        }
     }
 
     return (
@@ -44,18 +27,12 @@ export default function ConfigAboutPage() {
                 <code>{__COMMIT_HASH__}</code>
             </Box>
             <Box>
-                Service Workers: {swCount}
-            </Box>
-            <Box>
                 <List>
                     <ListItem>
                         <ListItemButton onClick={() => reload()}>Reload</ListItemButton>
                     </ListItem>
                     <ListItem>
                         <ListItemButton onClick={() => clearCacheAndReload()}>Clear Cache and Reload</ListItemButton>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton onClick={() => forceSwUpdate()}>Force Service Worker Update</ListItemButton>
                     </ListItem>
                 </List>
             </Box>
