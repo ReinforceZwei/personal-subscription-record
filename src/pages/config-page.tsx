e63a456c0eb3a4363b7a58762d5bf8a55a70322e
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2025-03-04 15:59:12 +0800

    feat: new UI design for configs

--- src/pages/config-page.tsx
+++ src/pages/config-page.tsx
@@ -1,19 +1,22 @@
-import { Autocomplete, Box, Tab, Tabs, TextField } from "@mui/material"
+import { Box, List, ListItem, ListItemButton, useMediaQuery } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Outlet, NavLink, useLocation, useNavigate } from "react-router-dom"
 import { selectLastConfigPage, setLastConfigPage } from "../redux/routingSlice"
 import { useAppDispatch } from "../hooks"
+import MobileLayout from "./config-pages/layouts/mobile-layout"
+import DesktopLayout from "./config-pages/layouts/desktop-layout"
 
-type ConfigPages = 'type' | 'payment' | 'preference' | 'budget' | 'preset' | 'about'
+export type ConfigPages = 'type' | 'payment' | 'preference' | 'budget' | 'preset' | 'about'
 
 export default function ConfigPage() {
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
+    const isMobileScreen = useMediaQuery('(max-width: 600px)')
 
     const { pathname } = useLocation()
     const lastPage = useSelector(selectLastConfigPage) as ConfigPages
-    const [value, setValue] = useState<ConfigPages>(lastPage || 'preference')
+    const [value, setValue] = useState<ConfigPages>(lastPage || (isMobileScreen ? '' : 'preference'))
     
 
     useEffect(() => {
@@ -30,29 +33,22 @@ export default function ConfigPage() {
         } else if (pathname.includes('/about')) {
             setValue('about')
         } else {
-            navigate(lastPage)
+            //navigate(lastPage)
         }
     }, [value, pathname, lastPage])
+
+    useEffect(() => {
+        if (!isMobileScreen) {
+            navigate(`/config/${value}`)
+        }
+    }, [value, isMobileScreen])
 
     useEffect(() => {
         dispatch(setLastConfigPage(value))
     }, [value])
 
-    return (
-        <div>
-            <Box mb={2}>
-                <Tabs value={value} onChange={(e, v) => setValue(v)} role='navigation' variant="scrollable" scrollButtons="auto">
-                    <Tab component={NavLink} to='preference' value='preference' label='喜好' />
-                    <Tab component={NavLink} to='budget' value='budget' label='預算' />
-                    <Tab component={NavLink} to='type' value='type' label='支出類別' />
-                    <Tab component={NavLink} to='payment' value='payment' label='支付方式' />
-                    <Tab component={NavLink} to='preset' value='preset' label='預設範本' />
-                    <Tab component={NavLink} to='about' value='about' label='關於' />
-                </Tabs>
-            </Box>
-            <div>
-                <Outlet />
-            </div>
-        </div>
-    )
+    if (isMobileScreen) {
+        return <MobileLayout />
+    }
+    return <DesktopLayout />
 }
